75f71981a0899de94bf07061c1d7a78d568e9526
test <xxx@google.com>
2020-01-03 14:49:50 +0700

    lab4_1

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
 
+int main()
+{
+    double y;
+    int x;
+    cout<<"Enter x = ";
+    cin>>x;
+    y=3*(pow(x,3))+2*2.71828+pow(2,(2*x)+1)+sqrt(pow(x,2)+1);
+    cout<<"Result y = "<<y;
+    return 0;
+}
